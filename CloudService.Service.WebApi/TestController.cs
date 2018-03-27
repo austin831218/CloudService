6ab6010536bc158331b015bdf70c3774b592ce3c
Austin Wu <austin831218@gmail.com>
2018-03-27 16:06:22 +0800

    wire up logger in controller

--- CloudService.Service.WebApi/TestController.cs
+++ CloudService.Service.WebApi/TestController.cs
@@ -8,7 +8,16 @@ namespace CloudService.Service.WebApi
     [Route("api/[controller]")]
     public class TestController : Controller
     {
+        private ILogger _log;
+        public TestController(ILogger<TestController> log)
+        {
+            _log = log;
+        }
         [HttpGet]
-        public async Task<IActionResult> Get() => Ok(new[] { "value1", "value2" });
+        public async Task<IActionResult> Get()
+        {
+            _log.LogInformation("get values");
+            return Ok(new[] { "value1", "value2" });
+        }
     }
 }
